490c55e38f566ee3262c4f24710b1bee3810f0a7
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-03-11 13:17:40 +0200

    added TokenService

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import TokenService from './token'
 
-// Create axios instance with base configuration
-const api = axios.create({
+// Create axios instance
+const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -9,31 +10,66 @@ const api = axios.create({
   },
 })
 
-// Request interceptor to add token
-api.interceptors.request.use(
+// Request interceptor
+instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('hostaway_token')
+    const token = TokenService.getToken()
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
     return config
   },
-  (error) => {
-    return Promise.reject(error)
-  },
+  (error) => Promise.reject(error),
 )
 
-// Response interceptor for error handling
-api.interceptors.response.use(
+// Response interceptor
+instance.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      // Token expired - redirect to login or refresh token
-      console.log('Token expired, need to get new token')
-      // You could trigger token refresh here
+  async (error) => {
+    const originalRequest = error.config
+
+    if (!error.response || error.response.status !== 401 || originalRequest._retry) {
+      return Promise.reject(error)
     }
-    return Promise.reject(error)
+
+    originalRequest._retry = true
+
+    try {
+      // Make refresh request
+      const response = await instance.post(
+        'https://api.hostaway.com/v1/accessTokens',
+        new URLSearchParams({
+          grant_type: 'client_credentials',
+          client_id: process.env.VUE_APP_CLIENT_ID,
+          client_secret: process.env.VUE_APP_CLIENT_SECRET,
+          scope: 'general',
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cache-Control': 'no-cache',
+          },
+        },
+      )
+
+      const { access_token } = response.data
+
+      // Store token
+      TokenService.setToken(access_token)
+
+      // Update headers
+      instance.defaults.headers.common['Authorization'] = `Bearer ${access_token}`
+
+      // Retry original with new token
+      originalRequest.headers.Authorization = `Bearer ${access_token}`
+      return instance(originalRequest)
+    } catch (refreshError) {
+      TokenService.deleteToken()
+
+      window.location.href = '/'
+      return Promise.reject(refreshError)
+    }
   },
 )
 
-export default api
+export default instance
